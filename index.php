58c84fd56734b69af92d6c1c2188542e3872badd
Felipe <felipe.antinho@sudema.pb.gov.br>
2023-11-03 13:48:45 -0300

    feat: video to mp3 conversion

--- index.php
+++ index.php
@@ -3,29 +3,44 @@
 // header("Content-Type: application/json");
 
 if (isset($_FILES)) {
-    move_uploaded_file($_FILES['file']['tmp_name'], 'videos/teste.mp4');
+    $filename = str_replace(" ", "_", microtime());
+    move_uploaded_file($_FILES['file']['tmp_name'], "videos/$filename.mp4");
 
     $pipes = [
         1 => ["pipe", "w"],
-        2 => ["file", "error.log", "a"]
+        2 => ["file", "stdout.log", "a"]
     ];
     $p = [];
-    $process = proc_open('ffmpeg -i videos/teste.mp4 -preset ultrafast -threads 24 -f wav pipe:1', $pipes, $p);
+    $process = proc_open("ffmpeg -i videos/$filename.mp4 -preset ultrafast -threads 24 -q:a 0 -map a -f mp3 pipe:1", $pipes, $p);
 
     if (is_resource($process)) {
-        $final_file = "data:audio/wav;base64,";
-        $offset = 1048;
+        $final_file = "";
 
         while (!feof($p[1])) {
-            $final_file .= base64_encode(stream_get_contents($p[1], $offset));
-            $offset += 1000;
+            $final_file .= base64_encode(stream_get_contents($p[1]));
         }
 
-        // echo json_encode($final_file, JSON_UNESCAPED_SLASHES);
-        echo "<audio controls='controls' autobuffer='autobuffer' autoplay='autoplay'>
-        <source src='$final_file' />
-    </audio>";
-        fclose($p[1]);
+        echo "
+        <iframe width='500' height='500' id='audio'></iframe>
+        <script>
+        var base64String = '$final_file';
+        var byteCharacters = atob(base64String);
+        var byteNumbers = new Array(byteCharacters.length);
+        for (var i = 0; i < byteCharacters.length; i++) {
+           byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        var byteArray = new Uint8Array(byteNumbers);
+        var blob = new Blob([byteArray], {type: 'audio/mp3'});
+        var url = URL.createObjectURL(blob);
+        
+        var audio = document.getElementById('audio');
+        audio.src = url;
+        </script>";
+        
+        if (feof($p[1])) {
+            fclose($p[1]);
+            unlink("videos/$filename.mp4");
+        }
     }
 } else {
     echo json_encode(["error" => "Você precisa enviar um arquivo de vídeo."]);
