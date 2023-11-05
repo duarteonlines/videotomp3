2ead40b503510c6978c912a6a5be6f843bfc2351
Felipe <felipe.antinho@sudema.pb.gov.br>
2023-11-05 11:32:06 -0300

    feat: add customizations

--- index.php
+++ index.php
@@ -2,45 +2,87 @@
 
 // header("Content-Type: application/json");
 
-if (isset($_FILES)) {
-    $filename = str_replace(" ", "_", microtime());
-    move_uploaded_file($_FILES['file']['tmp_name'], "videos/$filename.mp4");
+if (isset($_POST['submit'], $_POST['format_type'], $_POST['quality'], $_POST['compression_preset'], $_FILES['file'])) {
 
-    $pipes = [
-        1 => ["pipe", "w"],
-        2 => ["file", "stdout.log", "a"]
+    $format_type = escapeshellcmd($_POST['format_type']);
+    $compression_preset = escapeshellcmd($_POST['compression_preset']);
+    $quality = escapeshellcmd($_POST['quality']);
+    $file = $_FILES['file'];
+    $validation_schema = [
+        'format_types_accepted' => [
+            "mp3",
+            "wav"
+        ],
+        'mimetypes_permited' => [
+            "video/mp4"
+        ],
+        'presets_accepted' => [
+            'slow',
+            'medium',
+            'ultrafast'
+        ],
+        'quality' => [
+            '17',
+            '34',
+            '51'
+        ]
     ];
-    $p = [];
-    $process = proc_open("ffmpeg -i videos/$filename.mp4 -preset ultrafast -threads 24 -q:a 0 -map a -f mp3 pipe:1", $pipes, $p);
 
-    if (is_resource($process)) {
-        $final_file = "";
+    // TODO: refactor
+    if (in_array($file["type"], $validation_schema['mimetypes_permited']) && in_array($format_type, $validation_schema['format_types_accepted']) && in_array($compression_preset, $validation_schema['presets_accepted']) && in_array($quality, $validation_schema['quality'])) {
 
-        while (!feof($p[1])) {
-            $final_file .= base64_encode(stream_get_contents($p[1]));
+        $file_extension = explode("/", $file["type"])[1];
+        $filename = 'videos/' . str_replace(" ", "_", microtime()) . $file_extension;
+        $is_file_moved = move_uploaded_file($_FILES['file']['tmp_name'], $filename);
+
+        if (!$is_file_moved) {
+            header("HTTP 1.1 500 Internal Server Error", 500);
+            echo json_encode(["error" => "Um erro interno ocorreu. Contate o Suporte."]);
+            die();
         }
 
-        echo "
-        <iframe width='500' height='500' id='audio'></iframe>
-        <script>
-        var base64String = '$final_file';
-        var byteCharacters = atob(base64String);
-        var byteNumbers = new Array(byteCharacters.length);
-        for (var i = 0; i < byteCharacters.length; i++) {
-           byteNumbers[i] = byteCharacters.charCodeAt(i);
+        $pipes = [
+            1 => ["pipe", "w"],
+            2 => ["file", "stdout.log", "a"]
+        ];
+        $p = [];
+
+        $process = proc_open("ffmpeg -i $filename -preset $compression_preset -crf $quality -threads 24 -q:a 0 -map a -f $format_type pipe:1", $pipes, $p);
+
+        if (is_resource($process)) {
+            $final_file = "";
+
+            while (!feof($p[1])) {
+                $final_file .= base64_encode(stream_get_contents($p[1]));
+            }
+
+            echo "
+            <iframe width='500' height='500' id='audio'></iframe>
+            <script>
+            var base64String = '$final_file';
+            var byteCharacters = atob(base64String);
+            var byteNumbers = new Array(byteCharacters.length);
+            for (var i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+            var byteArray = new Uint8Array(byteNumbers);
+            var blob = new Blob([byteArray], {type: 'audio/$format_type'});
+            var url = URL.createObjectURL(blob);
+            
+            var audio = document.getElementById('audio');
+            audio.src = url;
+            </script>";
+
+            // echo json_encode($final_file, JSON_UNESCAPED_SLASHES);
+
+            if (feof($p[1])) {
+                fclose($p[1]);
+                unlink($filename);
+            }
         }
-        var byteArray = new Uint8Array(byteNumbers);
-        var blob = new Blob([byteArray], {type: 'audio/mp3'});
-        var url = URL.createObjectURL(blob);
-        
-        var audio = document.getElementById('audio');
-        audio.src = url;
-        </script>";
-        
-        if (feof($p[1])) {
-            fclose($p[1]);
-            unlink("videos/$filename.mp4");
-        }
+    } else {
+        header("400 Bad Request", 400);
+        echo json_encode(["error" => "Arquivo ou formato de saída inválido"]);
     }
 } else {
     echo json_encode(["error" => "Você precisa enviar um arquivo de vídeo."]);
--- teste.php
+++ teste.php
@@ -8,7 +8,28 @@
 
     <form action="index.php" method="post" enctype="multipart/form-data">
         <label for="file"><span>Filename:</span></label>
-        <input type="file" name="file" id="file" />
+        <input type="file" required name="file" id="file" />
+        <br />
+        <label for="format">Selecione o formato de saida:</label>
+        <select name="format_type" id="format" required>
+            <option value="mp3" selected>MP3</option>
+            <option value="wav">WAV</option>
+        </select>
+        <br />
+        <label for="compression_level">Nível de compressão:</label>
+        <select name="compression_preset" id="compression_level" required>
+            <option value="slow">Muito bom</option>
+            <option value="medium" selected>Normal</option>
+            <option value="ultrafast">Ruim</option>
+        </select>
+        <br />
+        <label for="quality">Qualidade:</label>
+        <select name="quality" id="quality" required>
+            <option value="51">Péssima</option>
+            <option value="34" selected>Ruim</option>
+            <option value="17">Normal</option>
+            <option value="0">Muito boa</option>
+        </select>
         <br />
         <input type="submit" name="submit" value="Submit" />
     </form>
