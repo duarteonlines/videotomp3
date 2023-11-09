ff181fa18a49569907bfc4e3b3f12ddd91cb2202
Felipe <felipe.antinho@sudema.pb.gov.br>
2023-11-09 14:54:48 -0300

    feat: add camadas

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-}
+require_once __DIR__ . "/middlewares/cors.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    exit(0);
-}
+cors();
+
+// mandar pros controllers
 
 if (isset($_POST['format_type'], $_POST['quality'], $_POST['compression_preset'], $_FILES['file'])) {
     $format_type = escapeshellcmd($_POST['format_type']);
